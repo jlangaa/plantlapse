87659c2e7e61f67355aab982a2b287ccbd3a750a
jal <me@josh-langfus.com>
2021-01-02 10:54:28 -0500

    fixed time comparison bug

--- timelapse.sh
+++ timelapse.sh
@@ -2,20 +2,20 @@
 
 # Wrapper for timelapse.py to be used with cron
 
-t=$(date +%H:%M)
+t=$(date +%H%M)
 
 python3 ~/Projects/plantlapse/lightswitch.py on
 
-python3 ~/Projects/plantlapse/plantlapse.py ~/plantlapse_data
+#python3 ~/Projects/plantlapse/plantlapse.py ~/plantlapse_data
 
-if [[ $t > 23:00 || $t < 8:00 ]]; then
+if [[ $t -gt 2300 || $t -lt 800 ]]; then
 	echo $(date)": Turning off the light"
 	python3 ~/Projects/plantlapse/lightswitch.py off	
 	echo $(date)": Complete"
 fi
 
 echo $(date)": Converting images to GIF"
-convert -delay 20 -resize 40% -loop 0 ~/plantlapse_data/img*.jpg ~/plantlapse_data/timelapse.gif
+#convert -delay 20 -resize 40% -loop 0 ~/plantlapse_data/img*.jpg ~/plantlapse_data/timelapse.gif
 echo $(date)": Complete"
 
 echo "================================================================================"
